f7b0a48db5472f20c0691ca6ccd698243472b0f0
bo40 <bo40.git@gmail.com>
2019-07-01 00:52:10 +0900

    modify typo at strftime

--- backup/kumanodocs_big_backup.sh
+++ backup/kumanodocs_big_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BACKUP_DIR=/var/backup
-BACKUP_PATH=${BACKUP_DIR}/kumanodocs-$(date +"%Y-%m-&d").bak.pg
+BACKUP_PATH=${BACKUP_DIR}/kumanodocs-$(date +"%Y-%m-%d").bak.pg
 
 # backup localy and upload to dropbox
 pg_dump -U kd -T users -aFc kumanodocs > ${BACKUP_PATH}
--- backup/kumanodocs_small_backup.sh
+++ backup/kumanodocs_small_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BACKUP_DIR=/var/backup
-BACKUP_PATH=${BACKUP_DIR}/small-kumanodocs-$(date +"%Y-%m-&d").bak.pg
+BACKUP_PATH=${BACKUP_DIR}/small-kumanodocs-$(date +"%Y-%m-%d").bak.pg
 
 # backup localy, don't upload
 pg_dump -U kd -T users -aFc kumanodocs > ${BACKUP_PATH}
